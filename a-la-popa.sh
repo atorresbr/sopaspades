88662b89349012be36aad063a1c2ad2b6650258a
Anderson Torres <atorresnh@gmail.com>
2024-03-03 09:15:55 -0300

    A La Popa Bash Shell Script 🍜

---
+++ a-la-popa.sh
@@ -0,0 +1,98 @@
+#bin/bash
+## La Popa és Nuestra !
+
+## informand sobre a instalação
+
+echo
+echo
+sleep 1
+echo -ne " 🇪🇸 Este script instalará Sopa Spades en tu PC 🍜"
+echo
+sleep 1
+echo -ne " 🇺🇸 This scrip will isntall Sopa Spades on you PC 🍜"
+echo
+sleep 1
+echo -ne " 🇧🇷 Este script irá instalar o Sopa Spades em seu PC 🍜"
+echo
+
+## informando sobre as dependências
+
+sleep 1
+echo -ne " 🇪🇸 Instalando las dependencias"
+echo
+sleep 1
+echo -ne " 🇺🇸 Installing the dependencies"
+echo
+sleep 1
+echo -ne " 🇧🇷 Instalando as dependências"
+echo
+
+
+sleep 1
+echo " sudo apt-get install pkg-config libglew-dev libcurl3-openssl-dev libsdl2-dev \
+     libsdl2-image-dev libalut-dev xdg-utils libfreetype6-dev libopus-dev \
+     libopusfile-dev cmake imagemagick \
+     libjpeg-dev libxinerama-dev libxft-dev "
+
+echo
+sleep 1
+
+ sudo apt-get install pkg-config libglew-dev libcurl3-openssl-dev libsdl2-dev \
+     libsdl2-image-dev libalut-dev xdg-utils libfreetype6-dev libopus-dev \
+     libopusfile-dev cmake imagemagick \
+     libjpeg-dev libxinerama-dev libxft-dev && \
+
+## Clonando os repositórios
+
+sleep 1
+echo -ne " 🇪🇸 Clonando el repositorio y instalando"
+echo
+sleep 1
+echo -ne " 🇺🇸 Cloning the repository and installing"
+echo
+sleep 1
+echo -ne " 🇧🇷 Clonando o repositório e instalando"
+echo
+
+sleep 1
+echo "   sudo rm -Rf sopaspades && sudo rm -Rf openspades && \
+   git clone https://github.com/atorresbr/sopaspades.git && cd sopaspades "
+
+sleep 1
+
+   sudo rm -Rf sopaspades && sudo rm -Rf openspades && \
+   git clone https://github.com/atorresbr/sopaspades.git && cd sopaspades && \
+
+sleep 1
+echo -ne " 🇪🇸 instalando"
+echo
+sleep 1
+echo -ne " 🇺🇸 installing"
+echo
+sleep 1
+echo -ne " 🇧🇷 instalando"
+echo
+
+sleep 1
+
+echo "    mkdir sopaspades.mk && cd sopaspades.mk && cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo && \
+   make && sudo make install "
+
+   mkdir sopaspades.mk && cd sopaspades.mk && cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo && \
+   make && sudo make install && \
+
+sleep 1
+echo -ne " 🇪🇸 comenzando el Juego"
+echo
+sleep 1
+echo -ne " 🇺🇸 starting the Game"
+echo
+sleep 1
+echo -ne " 🇧🇷 iniciando o Jogo"
+echo
+
+sleep 1
+
+ echo " openspades "
+
+  openspades
